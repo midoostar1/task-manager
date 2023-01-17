56abf055dfbb81c45fe4faa7adc8b9f81f1daf23
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-01-16 22:24:27 -0600

    init

---
+++ src/test/java/com/example/task_mamager/TaskMamagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_mamager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMamagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
