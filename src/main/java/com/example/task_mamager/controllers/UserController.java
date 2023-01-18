4e5f096a80ac39ea8121341e15fcc1a854a91e2d
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-01-18 12:33:48 -0600

    sign up functionality completed, about page completed

--- src/main/java/com/example/task_mamager/controllers/UserController.java
+++ src/main/java/com/example/task_mamager/controllers/UserController.java
@@ -4,32 +4,102 @@ package com.example.task_mamager.controllers;
 import com.example.task_mamager.models.User;
 import com.example.task_mamager.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @AllArgsConstructor
 @Controller
 
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public boolean isValidPassword(String password) {
+
+        return password.length() >= 8 &&
+
+                password.contains("0") ||
+                password.contains("1") ||
+                password.contains("3") ||
+                password.contains("4") ||
+                password.contains("5") ||
+                password.contains("6") ||
+                password.contains("7") ||
+                password.contains("8") ||
+                password.contains("9")
+                        &&
+                        password.contains("!") ||
+                password.contains("#") ||
+                password.contains("$") ||
+                password.contains("%") ||
+                password.contains("&") ||
+                password.contains("*") ||
+                password.contains("-") ||
+                password.contains("_") ||
+                password.contains("(") ||
+                password.contains("+") ||
+                password.contains(")") ||
+                password.contains("=") ||
+                password.contains("{") ||
+                password.contains("}") ||
+                password.contains("[") ||
+                password.contains("]") ||
+                password.contains(":") ||
+                password.contains(";") ||
+                password.contains("\"") ||
+                password.contains("<") ||
+                password.contains(">") ||
+                password.contains(".") ||
+                password.contains("?") ||
+                password.contains("@") ||
+                password.contains("\\") ||
+                password.contains("'");
+
+
+    }
 
 
     @GetMapping("/signup")
-    public String showRegisterForm(Model model)
-    {
+    public String showRegisterForm(Model model) {
         model.addAttribute("user", new User());
         return "register";
     }
 
     @PostMapping("/signup")
-    public String addUser( @ModelAttribute User user){
+    public String addUser(@Validated @ModelAttribute User user, BindingResult br, Model model, RedirectAttributes attributes) {
+        String password = user.getPassword();
+
+        if (br.hasErrors()) {
+            return "register";
+        }
+        if (!isValidPassword(password)) {
+            model.addAttribute("invalid", "Password must be 8 characters and above and must contain a special character and and at least 1 integer.");
+                return "register";
+        }
+
+
+        String encodedPassword = passwordEncoder.encode(password);
+        user.setPassword(encodedPassword);
 
         userService.save(user);
+        attributes.addFlashAttribute("success", "You are successfully registered! You can now login.");
+
         return "redirect:/login";
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "profile";
     }
 
 }
--- src/main/java/com/example/task_mamager/models/User.java
+++ src/main/java/com/example/task_mamager/models/User.java
@@ -31,7 +31,8 @@ public class User {
    @Column(nullable = false, unique = true)
     private String email;
 
-   @Column(nullable = false)
+
+
     private String password;
 
     @Column(name = "profile_photo")
