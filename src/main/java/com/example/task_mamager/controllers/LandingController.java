98f7a97fdf8f1fefdadb000cbc890d856f17af3c
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-01-20 10:42:24 -0600

    added a user to seeder file

--- src/main/java/com/example/task_mamager/controllers/LandingController.java
+++ src/main/java/com/example/task_mamager/controllers/LandingController.java
@@ -1,14 +1,24 @@
 package com.example.task_mamager.controllers;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@AllArgsConstructor
 @Controller
 public class LandingController {
 
+  private final PasswordEncoder passwordEncoder;
+
     @GetMapping("/")
     public String showLanding(){
+    String password = "password1234" ;
+    String encode = passwordEncoder.encode(password);
+        System.out.println(encode);
+
         return "index";
     }
 }
