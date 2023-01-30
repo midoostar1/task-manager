3cf374f888d1d2997bd4087bd92f88e34eb17c77
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-01-30 05:11:32 -0600

    styling

--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -3,7 +3,9 @@
 $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
+        console.log("clicked")
         $('#sidebar').toggleClass('active');
+        $('#main-content').toggleClass('active');
     });
 
 });
